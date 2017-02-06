c6032cd6f3ca189575ae85063e5c152a189efd05
Elsy Sugeli Pinzón Vivas <espinzonv@MacBook-Air-de-Elsy.local>
2017-02-06 09:32:51 -0600

    first commit

---
+++ line/naive_line.c
@@ -0,0 +1,51 @@
+/*A line drawing algorithm is a graphical algorithm for approximating a line segment on discrete graphical media. */
+
+#include<stdio.h>
+#include<math.h>
+
+#define MAX_X 1920
+#define MAX_Y 1080
+#define MAX(a,b) ((a) > (b) ? a : b)
+#define MIN(a,b) ((a) < (b) ? a : b)
+
+int raster[MAX_X][MAX_Y];
+
+
+void printRaster(int maxX, int maxY){
+	for(int y=maxY; y>=0; y--){
+		for(int x=0; x<=maxX; x++){
+			if(raster[x][y]==0){
+				printf("'");
+			} else printf("*");
+		} printf("\n");
+	}
+}
+
+int main(){
+	
+	int x0,y0, x1,y1;
+	
+	printf("Write the first point\n");
+	scanf("%d %d", &x0, &y0);
+	printf("Write the second point\n");
+	scanf("%d %d", &x1, &y1);
+
+	double dy,dx,m;
+	dy=y0-y1;
+	dx= x0-x1;
+	//validate
+	m=(dy/dx);
+
+	double b;
+	b= y0 - m*x0;
+
+	int y;
+	for(int x=x0; x<=x1; x++){
+		y = round((m*x)+b);
+		raster[x][y]=1;
+		printf("( %d , %d )\n",x,y);
+	}
+
+	printRaster(MAX(x0,x1), MAX(y0,y1));
+
+}
