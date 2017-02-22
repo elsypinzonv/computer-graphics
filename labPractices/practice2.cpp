823d99f4b013ddeefbe47c6041d87244464da499
elsypinzonv <elsy_pinzonv@hotmail.com>
2017-02-22 12:33:32 -0600

    Create practice2

---
+++ labPractices/practice2.cpp
@@ -0,0 +1,24 @@
+/*Generate a Random FULL HS Image*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int getRandomNumber(){
+  return rand() % 256;
+}
+
+int main(){
+	srand(time(NULL));
+	printf("P3\n");
+	printf("1920 1080\n");
+	printf("255\n");
+	for(int j=0; j<1080; j++){
+		for(int i=0; i<1920; i++){
+			printf("%d\n ", getRandomNumber());
+			printf("%d\n ", getRandomNumber());
+			printf("%d\n ", getRandomNumber());
+		}
+		printf("\n");
+	}
+	return 0;
+}
